5baf1ad54aba34e298cd2bbcf3a86050eac05c06
Michael Cheng <mcheng.work@gmail.com>
2019-08-27 20:55:44 +0800

    Move to a shared method.

--- src/routes/events.js
+++ src/routes/events.js
@@ -6,11 +6,8 @@ const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 const moment = require('moment')
 
-/* List all events */
-router.get('/', function (req, res, next) {
-  const startDate = moment().hour(0).minute(0)
-
-  db.Event
+async function fetchEvents (startDate) {
+  return await db.Event
     .findAll({
       where: {
         active: true,
@@ -22,37 +19,42 @@ router.get('/', function (req, res, next) {
         ['start_time', 'ASC']
       ]
     })
-    .then(events => {
-      const eventListing = events.map(event => {
-        return {
-          id: event.platform_identifier,
-          name: event.name,
-          description: event.description,
-          location: event.location,
-          rsvp_count: event.reavp_count,
-          url: event.url,
-          group_id: event.group_id,
-          group_name: event.group_name,
-          group_url: event.group_url,
-          formatted_time: event.formatted_time,
-          unix_start_time: moment(event.start_time).unix(),
-          start_time: event.start_time,
-          end_time: event.end_time,
-          platform: event.platform,
-          rsvp_count: event.rsvp_count
-        }
-      })
+}
 
-      res.json({
-        meta: {
-          generated_at: moment().toISOString(),
-          location: 'Singapore',
-          api_version: 'v1',
-          total_events: events.length
-        },
-        events: eventListing
-      })
-    })
+/* List all events */
+router.get('/', async function (req, res, next) {
+  const startDate = moment().hour(0).minute(0)
+  const events = await fetchEvents(startDate)
+
+  const eventListing = events.map(event => {
+    return {
+      id: event.platform_identifier,
+      name: event.name,
+      description: event.description,
+      location: event.location,
+      rsvp_count: event.reavp_count,
+      url: event.url,
+      group_id: event.group_id,
+      group_name: event.group_name,
+      group_url: event.group_url,
+      formatted_time: event.formatted_time,
+      unix_start_time: moment(event.start_time).unix(),
+      start_time: event.start_time,
+      end_time: event.end_time,
+      platform: event.platform,
+      rsvp_count: event.rsvp_count
+    }
+  })
+
+  res.json({
+    meta: {
+      generated_at: moment().toISOString(),
+      location: 'Singapore',
+      api_version: 'v1',
+      total_events: events.length
+    },
+    events: eventListing
+  })
 })
 
 module.exports = router
