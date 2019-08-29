fc16fa7f692b6ea70a42643e87959a752b4373c7
Michael Cheng <mcheng.work@gmail.com>
2019-08-29 22:34:25 +0800

    Use Throng worker for background job worker

---
+++ src/tasks/harvest_event_worker.js
@@ -0,0 +1,95 @@
+require('dotenv').config()
+
+const throng = require('throng')
+
+const Queue = require('bull')
+const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379'
+
+const workers = process.env.WEB_CONCURRENCY || 1
+const maxJobsPerWorker = 20
+
+const db = require('../models/index')
+
+const HarvesterService = require('../services/harvester_service')
+const moment = require('moment-timezone')
+
+function start () {
+  const harvester = new HarvesterService({
+    meetup: {
+      consumerKey: process.env.MEETUP_OAUTH_KEY,
+      consumerSecret: process.env.MEETUP_OAUTH_SECRET,
+      refreshToken: process.env.MEETUP_REFRESH_TOKEN
+    }
+  })
+
+  const workQueue = new Queue('esg_events', REDIS_URL)
+
+  workQueue.process(maxJobsPerWorker, async (job, done) => {
+    try {
+      const allGroupEvents = []
+      await harvester.prepareService()
+
+      Promise.all([harvester.fetchGroupEvents(job.data)])
+        .then((allEventResponses) => {
+          allEventResponses.forEach((eventResponse) => {
+            allGroupEvents.push(...eventResponse.events)
+          })
+
+          console.log('=====================================================')
+          console.log(`Harvested ${allGroupEvents.length} events from ${job.data.urlname}`)
+          allGroupEvents.forEach((item) => {
+            console.log('Event:', item.name)
+
+            db.Event
+              .findOrBuild({
+                where: {
+                  platform: 'meetup',
+                  platform_identifier: `${item.id}`
+                }
+              })
+              .then(([event, created]) => {
+                let location = ''
+                if (item.venue) {
+                  location = item.venue.name
+
+                  if (item.venue.address_1) {
+                    location += `, ${item.venue.address_1}`
+                  }
+                }
+
+                const startTime = moment(`${item.local_date} ${item.local_time} +08:00`, 'YYYY-MM-DD HH:mm Z')
+                const endTime = moment(startTime).add(item.duration, 'milliseconds')
+
+                event.update({
+                  name: item.name,
+                  platform: 'meetup',
+                  platform_identifier: `${item.id}`,
+                  description: item.description,
+                  location: location,
+                  rsvp_count: item.yes_rsvp_count,
+                  url: item.link,
+                  group_id: item.group.id,
+                  group_name: item.group.name,
+                  group_url: `https://www.meetup.com/${item.group.urlname}`,
+                  formatted_time: startTime.tz('Asia/Singapore').format('DD MMM YYYY, ddd, h:mm a'),
+                  start_time: startTime.toDate(),
+                  end_time: endTime.toDate(),
+                  latitude: (item.venue ? item.venue.lat : null),
+                  longitude: (item.venue ? item.venue.lon : null)
+                }).then(() => {
+                  console.log('Updated the record for', item.name)
+                })
+              })
+          })
+        })
+        .finally(() => {
+          done()
+        })
+    } catch (err) {
+      console.log('Harvester Error:', err)
+      done(err)
+    }
+  })
+}
+
+throng({ workers, start })
--- src/tasks/harvest_events.js
+++ src/tasks/harvest_events.js
@@ -5,6 +5,8 @@ const db = require('../models/index')
 const Queue = require('bull')
 const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379'
 const workQueue = new Queue('esg_events', REDIS_URL)
+const delayInterval = 12000
+const batchSize = 2
 
 async function harvest () {
   console.log('Fetching all active groups')
@@ -17,26 +19,32 @@ async function harvest () {
     })
     console.log(`Found ${allActiveGroups.length} groups...`)
 
-    let allJobs = []
-    allActiveGroups.forEach(async (group) => {
+    let currentDelay = 0
+    const allJobs = allActiveGroups.map(async (group, index) => {
       console.log(`Adding job for ${group.name}...`)
+
+      console.log('Index', index)
+      if (((index + 1) % batchSize) === 0) {
+        currentDelay += delayInterval
+        console.log('Delay increased:', currentDelay)
+      }
 
       const jobDetails = {
         platform: group.platform,
         platform_identifier: group.platform_identifier,
         urlname: group.urlname
       }
-      allJobs.push(workQueue.add(jobDetails))
+      return workQueue.add(jobDetails, { delay: currentDelay })
     })
 
     Promise.all(allJobs)
-      .then( allJobResults => {
+      .then(allJobResults => {
         console.log('Number of added Jobs:', allJobResults.length)
       })
-      .catch( (err) => {
+      .catch((err) => {
         console.log('Job Add Errors', err)
       })
-      .finally( ()=>{
+      .finally(() => {
         console.log('Done.')
         workQueue.close()
         db.sequelize.close()
