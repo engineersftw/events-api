7d2c2d39a110dd6c03a1cf6c5b3c9daf3148ac29
Paul Joey Clark <joeytwiddle@gmail.com>
2020-11-05 13:31:06 +0800

    Add group name blacklist (insp. cheeaun)

--- src/tasks/filters.js
+++ src/tasks/filters.js
@@ -1,11 +1,22 @@
+const blacklistRegex = /(business|marketing|superbowl|blockchain)/i
+
 module.exports = {
 
   shouldRemoveGroup (group) {
-    return (
+    const probablyUnwanted = group.name.match(blacklistRegex)
+
+    if (probablyUnwanted) {
+      console.info(`Group ${group.name} was marked as unwated by heuristics`)
+    }
+
+    const onRejectList = (
       // Not tech related
       group.link === 'https://www.meetup.com/Kakis-SG-Anything-Watever-Meetup-Group/' ||
       group.link === 'https://www.meetup.com/Wireless-devices-and-your-health/'
     )
+    // No need to log these, since they were explicitly listed
+
+    return onRejectList || probablyUnwanted
   }
 
 }
