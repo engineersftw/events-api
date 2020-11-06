46f6384adc81f3a2af1f6813291ef80edba22740
Paul Joey Clark <joeytwiddle@gmail.com>
2020-11-06 14:05:46 +0800

    Move rejectlist into an array; add one entry

--- src/tasks/filters.js
+++ src/tasks/filters.js
@@ -1,20 +1,25 @@
-const blacklistRegex = /(business|marketing|superbowl|blockchain)/i
+const groupsToReject = [
+  // Not tech related
+  'https://www.meetup.com/Kakis-SG-Anything-Watever-Meetup-Group/',
+  'https://www.meetup.com/Wireless-devices-and-your-health/',
+  // I find this one a bit spammy, feels like a marketing drive
+  'https://www.meetup.com/A-US-stock-market-listedCo-Big-Data-AI-New-Technology/'
+]
+
+const probablyUnwantedRegex = /(business|marketing|superbowl|blockchain)/i
 
 module.exports = {
 
   shouldRemoveGroup (group) {
-    const probablyUnwanted = group.name.match(blacklistRegex)
-
-    if (probablyUnwanted) {
-      console.info(`Group ${group.name} was marked as unwated by heuristics`)
-    }
-
-    const onRejectList = (
-      // Not tech related
-      group.link === 'https://www.meetup.com/Kakis-SG-Anything-Watever-Meetup-Group/' ||
-      group.link === 'https://www.meetup.com/Wireless-devices-and-your-health/'
-    )
+    const onRejectList = groupsToReject.includes(group.link)
     // No need to log these, since they were explicitly listed
+
+    const probablyUnwanted = group.name.match(probablyUnwantedRegex)
+
+    if (probablyUnwanted && !onRejectList) {
+      // We log these, in case the admin wants to reconsider
+      console.info(`Group ${group.name} was marked as unwated by heuristics: ${group.link}`)
+    }
 
     return onRejectList || probablyUnwanted
   }
