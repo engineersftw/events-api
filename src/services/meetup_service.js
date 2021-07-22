005196f730581bcdf3d42f31257f2345dc5863cb
Paul Joey Clark <joeytwiddle@gmail.com>
2021-07-22 15:51:37 +0800

    Add a group to the blacklist

--- src/services/meetup_service.js
+++ src/services/meetup_service.js
@@ -178,7 +178,9 @@ class MeetupService {
       'https://www.meetup.com/A-US-stock-market-listedCo-Big-Data-AI-New-Technology/',
       // Cheap python courses ("crash course" and "full course"). Might be useful for new developers, but they are flooding our events list, and always the same thing!
       'https://www.meetup.com/Data-Science-and-Machine-Learning-University/',
-      'https://www.meetup.com/Teach-Code-for-Everyone/'
+      'https://www.meetup.com/Teach-Code-for-Everyone/',
+      // A bit spammy
+      'https://www.meetup.com/get-a-software-job-learn-coding-for-non-computer-sci-majors/'
     ]
   }
 }
