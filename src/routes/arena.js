274ed32cd88cad57d14b4f1a76611dd737c65d22
Michael Cheng <mcheng.work@gmail.com>
2021-08-01 18:08:08 +0800

    Do not verify SSL when in Heroku

--- src/routes/arena.js
+++ src/routes/arena.js
@@ -5,6 +5,21 @@ const auth = require('http-auth')
 const Arena = require('bull-arena')
 
 const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379'
+const url = require('url')
+const Redis = require('ioredis')
+
+const redisUri = url.URL(REDIS_URL)
+const redis = new Redis({
+  port: Number(redisUri.port) + 1,
+  host: redisUri.hostname,
+  password: redisUri.auth.split(':')[1],
+  db: 0,
+  tls: {
+    rejectUnauthorized: false,
+    requestCert: true,
+    agent: false
+  }
+})
 
 const basic = auth.basic({
   realm: 'Protected Area'
@@ -19,7 +34,7 @@ const arena = Arena({
     {
       name: 'esg_events',
       hostId: 'main_server',
-      url: REDIS_URL
+      url: redis
     }
   ]
 })
--- src/tasks/harvest_events.js
+++ src/tasks/harvest_events.js
@@ -8,8 +8,25 @@ if (process.env.SENTRY_DSN) {
 const db = require('../models/index')
 
 const Queue = require('bull')
+
 const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379'
-const workQueue = new Queue('esg_events', REDIS_URL)
+const url = require('url')
+const Redis = require('ioredis')
+
+const redisUri = url.URL(REDIS_URL)
+const redis = new Redis({
+  port: Number(redisUri.port) + 1,
+  host: redisUri.hostname,
+  password: redisUri.auth.split(':')[1],
+  db: 0,
+  tls: {
+    rejectUnauthorized: false,
+    requestCert: true,
+    agent: false
+  }
+})
+
+const workQueue = new Queue('esg_events', redis)
 const delayInterval = parseInt(process.env.EVENT_WORKER_DELAY_INTERVAL) || 12000
 const batchSize = parseInt(process.env.EVENT_WORKER_BATCH_SIZE) || 25
 
