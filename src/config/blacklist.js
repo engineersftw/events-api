6c0672c98769b85932e69c1d906bd0f796350292
lyqht <estee.tsw@gmail.com>
2022-03-26 10:55:56 +0800

    remove trailing slash in urls and add groups into blacklist

--- src/config/blacklist.js
+++ src/config/blacklist.js
@@ -1,14 +1,20 @@
 const blacklistedGroupUrls = [
   // Not tech related
-  'https://www.meetup.com/Kakis-SG-Anything-Watever-Meetup-Group/',
-  'https://www.meetup.com/Wireless-devices-and-your-health/',
+  'https://www.meetup.com/Kakis-SG-Anything-Watever-Meetup-Group',
+  'https://www.meetup.com/Wireless-devices-and-your-health',
   // I find this one a bit spammy, feels like a marketing drive
-  'https://www.meetup.com/A-US-stock-market-listedCo-Big-Data-AI-New-Technology/',
+  'https://www.meetup.com/A-US-stock-market-listedCo-Big-Data-AI-New-Technology',
   // Cheap python courses ("crash course" and "full course"). Might be useful for new developers, but they are flooding our events list, and always the same thing!
-  'https://www.meetup.com/Data-Science-and-Machine-Learning-University/',
-  'https://www.meetup.com/Teach-Code-for-Everyone/',
+  'https://www.meetup.com/Data-Science-and-Machine-Learning-University',
+  'https://www.meetup.com/Teach-Code-for-Everyone',
   // A bit spammy
-  'https://www.meetup.com/get-a-software-job-learn-coding-for-non-computer-sci-majors/'
+  'https://www.meetup.com/get-a-software-job-learn-coding-for-non-computer-sci-majors',
+  // This is not really tech specific
+  'https://www.meetup.com/Startup-Agile-Group-Singapore',
+  'https://www.meetup.com/Startup-Prodigy-Singapore',
+
+  // this group mentioned that they will keep postponing events until COVID is over
+  'https://www.meetup.com/Never-Code-Alone-SG'
 ]
 
 module.exports = { blacklistedGroupUrls }
