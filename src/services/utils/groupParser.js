664d9eb8a1556817a71862051370b5b44d5612e7
lyqht <estee.tsw@gmail.com>
2022-03-28 22:48:48 +0800

    axios error handling

--- src/services/utils/groupParser.js
+++ src/services/utils/groupParser.js
@@ -12,43 +12,46 @@ const getUrlName = (meetupGroupUrl) => {
 }
 
 const getGroupDetails = async (groupUrl) => {
-  const response = await axios.get(groupUrl)
-  if (response.status !== 200) {
-    console.warn(`Fail to get retrieve group details for: ${groupUrl}`)
-    return
-  }
-
-  const { data } = response
   try {
-    const dom = new JSDOM(data, { runScripts: 'dangerously' }) // the option is required for the apollo script to run in the JSDOM
-    const apolloState = dom.window.__APOLLO_STATE__.ROOT_QUERY
-    const groupQuery = Object.keys(apolloState)[0]
-    const id = groupQuery.match(/\d+/)[0]
-    const urlname = getUrlName(groupUrl)
-    const name = dom.window.document.querySelector('.groupHomeHeader-groupName').textContent
-
-    let description = ''
+    const response = await axios.get(groupUrl)
+    const { data } = response
     try {
-      description = turndownService.turndown(
-        dom.window.document.querySelector('#overview>.group-description--wrapper>div').innerHTML,
-        { headingStyle: 'atx' }
-      ).trim()
-    } catch (err) {
-      console.warn('Unable to fetch/convert group description from html to markdown', err)
-    }
+      const dom = new JSDOM(data, { runScripts: 'dangerously' }) // the option is required for the apollo script to run in the JSDOM
+      if (!dom.window.__APOLLO_STATE__) {
+        console.warn(`Cannot find Apollo state for: ${groupUrl}`)
+        return
+      }
+      const apolloState = dom.window.__APOLLO_STATE__.ROOT_QUERY
+      const groupQuery = Object.keys(apolloState)[0]
+      const id = groupQuery.match(/\d+/)[0]
+      const urlname = getUrlName(groupUrl)
+      const name = dom.window.document.querySelector('.groupHomeHeader-groupName').textContent
 
-    return {
-      name,
-      platform: 'meetup',
-      platform_identifier: id,
-      link: groupUrl,
-      urlname,
-      description,
-      active: true,
-      blacklisted: isBlacklisted(name)
+      let description = ''
+      try {
+        description = turndownService.turndown(
+          dom.window.document.querySelector('#overview>.group-description--wrapper>div').innerHTML,
+          { headingStyle: 'atx' }
+        ).trim()
+      } catch (err) {
+        console.warn('Unable to fetch/convert group description from html to markdown', err)
+      }
+
+      return {
+        name,
+        platform: 'meetup',
+        platform_identifier: id,
+        link: groupUrl,
+        urlname,
+        description,
+        active: true,
+        blacklisted: isBlacklisted(name)
+      }
+    } catch (err) {
+      console.error(`Error parsing group for ${groupUrl}: ${err}`)
     }
   } catch (err) {
-    console.error(`Error parsing group for ${groupUrl}: ${err}`)
+    console.error(`Axios error for ${groupUrl}: ${err}`)
   }
 }
 
